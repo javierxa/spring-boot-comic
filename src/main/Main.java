f82feafc79606cb7ba69c41196e11da255a04a2c
xavier <xavierdhoskins@gmail.com>
2023-12-26 18:09:24 -0600

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int num = 0;
+    int sum = 0;
+
+    String test = scanner.nextLine();
+
+        System.out.println(test);
+
+    }
+}
